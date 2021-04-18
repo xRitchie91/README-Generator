6519e4b97d7eb4b02282c6a391b77d679631b596
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-04-18 02:51:27 -0500

    updated questions, added license choices

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+
     {
         type: 'input',
         name: 'project',
@@ -22,26 +22,51 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'use',
+        name: 'usage',
         message: 'What is the application used for?'
     },
     {
         type: 'input',
-        name: '',
+        name: 'contributing',
         message: ''
     },
     {
         type: 'input',
-        name: '',
-        message: ''
+        name: 'tests',
+        message: 'Provide project test instructions'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Which license is your project covered under?',
+        choices: [
+            '[GNU General Public License](GNU-GP-LIC.txt)',
+            '[MIT License](MITLIC.txt)'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
     }
+]
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { },
 
 // TODO: Create a function to initialize app    
-function init() {}
+writeToFile.init(),
+{
 
-// Function call to initialize app
-init();
+},
+
+    // Function call to initialize app
+    init()
+
+    ,]
