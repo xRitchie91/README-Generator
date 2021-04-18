57f37b6268a02bf984165a356dd804668ec3f834
xRitchie91 <74946954+xRitchie91@users.noreply.github.com>
2021-04-18 02:11:54 -0500

    created questions for user input

---
+++ index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project called?'
+    },
+    {
+        type: 'input',
+        name: '',
+        message: ''
+    }
+    {
+        type: 'input',
+        name: '',
+        message: ''
+    }
+    {
+        type: '',
+        name: '',
+        message: ''
+    }
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app    
+function init() {}
+
+// Function call to initialize app
+init();
